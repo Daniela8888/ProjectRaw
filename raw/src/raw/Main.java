e1029088c58a1a57daad784e80fefac46521c4ee
Daniela8888 <32717667+Daniela8888@users.noreply.github.com>
2018-02-20 10:29:59 +0100

    Add files via upload

--- raw/src/raw/Main.java
+++ raw/src/raw/Main.java
@@ -1,6 +1,11 @@
 package raw;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String [] args) {
@@ -12,16 +17,29 @@ public class Main {
 
             byte[] buffer = new byte[1000];
 
-            FileInputStream inputStream =
-                    new FileInputStream(fileName);
+            FileInputStream inputStream = new FileInputStream(fileName);
 
 
             int total = 0;
             int nRead = 0;
             while((nRead = inputStream.read(buffer)) != -1) {
 
-                System.out.println(new String(buffer));
+                System.out.println(new String());
                 total += nRead;
+
+                Scanner scanner = new Scanner(System.in);
+                fileName=scanner.nextLine();
+
+                String pattern= "\\c{2}\d{3}\c{2}" || "\([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})"||    ;
+                Pattern r = Pattern.compile(pattern);      //pattern-class
+                Matcher m = r.matcher(FileInputStream);
+                if (m.find()){
+                    System.out.println("SPZ is correct");
+
+                }
+                else{
+                    throw  ;
+                }
             }
 
 
